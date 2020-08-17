19af76ae2498cfb7537a2706f3b0b643d73b4448
Germain Kataku <katakugermain@gmail.com>
2020-08-17 08:52:47 +0100

    actu

--- actualite/models.py
+++ actualite/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Actualite(models.Model):
     actu_title = models.CharField(max_length=300)
+    propostion_nom = models.CharField(max_length=300)
     lois_content = models.TextField()
     actu_image = models.ImageField(upload_to='actu_img/')
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.actu_title
+    
